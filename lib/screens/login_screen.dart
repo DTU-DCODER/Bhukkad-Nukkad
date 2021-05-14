48601c03318f84e527a49cc49dc0415bc4345f4d
Naman Malhotra <namalhotra333@gmail.com>
2021-05-14 14:34:52 +0530

    styled login page

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -3,58 +3,150 @@ import "package:flutter/material.dart";
 class LoginScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Column(
-        children: [
-          Container(
-            child: Image.asset("assets/images/logo.jpeg"),
-          ),
-          Container(
-            child: Column(
-              children: [
-                Text("LOGIN"),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.alternate_email
+    return SingleChildScrollView(
+      child: SafeArea(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 30,
+            ),
+            Container(
+              child: Image.asset("assets/images/logo.jpeg"),
+            ),
+            Container(
+              margin: EdgeInsets.only(top: 20),
+              padding: EdgeInsets.all(20),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "LOGIN",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Theme.of(context).primaryColor,
                     ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(labelText: "DTU ROLL NUMBER"),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.alternate_email,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "DTU ROLL NUMBER",
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            contentPadding: EdgeInsets.all(15),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColor,
+                      ),
+                      SizedBox(
+                        width: 10,
+                      ),
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            labelText: "PASSWORD",
+                            filled: true,
+                            enabledBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            focusedBorder: OutlineInputBorder(
+                              borderSide: BorderSide.none,
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            contentPadding: EdgeInsets.all(15),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                  Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                    TextButton(
+                      onPressed: () {},
+                      child: Text(
+                        "FORGOT PASSWORD?",
+                        style: TextStyle(fontSize: 14),
                       ),
                     )
-                  ],
-                ),
-                Row(
-                  children: [
-                    Icon(
-                      Icons.lock,
-                    ),
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(labelText: "PASSWORD"),
+                  ]),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Container(
+                    child: Center(
+                      child: ConstrainedBox(
+                        constraints:
+                            BoxConstraints.tightFor(height: 50, width: 300),
+                        child: ElevatedButton(
+                          onPressed: () {},
+                          child: Text(
+                            "LOGIN",
+                            style: TextStyle(color: Colors.white, fontSize: 20),
+                          ),
+                          style: ButtonStyle(
+                            shape:
+                                MaterialStateProperty.all<RoundedRectangleBorder>(
+                              RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(18.0),
+                              ),
+                            ),
+                          ),
+                        ),
                       ),
-                    )
-                  ],
-                ),
-                Text("FORGOT PASSWORD?"),
-                ElevatedButton(onPressed: () {}, child: Text("LOGIN"))
-              ],
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-          Container(
-            child: Row(
-              children: [
-                Text(
-                  "Don't have an account?",
-                ),
-                Text(
-                  "REGISTER.",
-                )
-              ],
+            SizedBox(height: 170,),
+            Container(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text(
+                      "REGISTER.",
+                      style: TextStyle(fontSize: 16),
+                    ),
+                  )
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
